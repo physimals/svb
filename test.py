a6fcb771e3a4f42c9a6e0737a9169180ad3d10cf
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-03-22 11:35:42 +0000

    Moved to another test script

--- test.py
+++ test.py
@@ -206,62 +206,11 @@ def test_onevox(model):
     plt.plot(t,y_est_post_corr,'b')
     plt.show()
 
-def test_nifti(fname, model, t0, dt, outdir="."):
-    """
-    Fit to a 4D Nifti image
-
-    :param fname: File name of Nifti image
-    :param mode: Model to fit to
-    :param t0: Timeseries first value
-    :param dt: Time step
-    """
-    nii = nib.load(fname)
-    d = nii.get_data()
-    shape = d.shape
-    d_flat = d.reshape(-1, shape[-1])
-
-    # Generate timeseries FIXME should be derivable from the model
-    t = np.linspace(t0, t0+shape[3]*dt, num=shape[3], endpoint=False)
-
-    # Train with no correlation between parameters
-    vae = VaeNormalFit(model, mode_corr='no_post_corr',
-                       learning_rate=LEARNING_RATE,
-                       draw_size=DRAW_SIZE)
-    _, cost_history_no_corr = vae.train(t, d_flat, batch_size=BATCH_SIZE, training_epochs=TRAINING_EPOCHS)
-    
-    # Transpose output so the first index is by parameter
-    means = np.transpose(vae.output("mp_mean"))
-    variances = np.transpose(np.diagonal(vae.output("mp_covar"), axis1=1, axis2=2))
-
-    # Write out parameter mean and variance images
-    makedirs(outdir, exist_ok=True)
-    for idx, param in enumerate(model.params):
-        nii_mean = nib.Nifti1Image(means[idx].reshape(shape[:3]), None, header=nii.get_header())
-        nii_mean.to_filename(os.path.join(outdir, "mean_%s.nii.gz" % param.name))
-        nii_var = nib.Nifti1Image(variances[idx].reshape(shape[:3]), None, header=nii.get_header())
-        nii_var.to_filename(os.path.join(outdir, "var_%s.nii.gz" % param.name))
-
-    # Write out modelfit
-    # FIXME currently have to tile parameters because require 1 value per time point in evaluate
-    tiled_params = np.tile(np.expand_dims(means, axis=-1), (1, 1, len(t)))
-    fit = model.ievaluate(tiled_params, t)
-    fit_nii = nib.Nifti1Image(fit.reshape(shape), None, header=nii.get_header())
-    fit_nii.to_filename(os.path.join(outdir, "modelfit.nii.gz"))
-
-def makedirs(d, exist_ok=False):
-    try:
-        os.makedirs(d)
-    except OSError as e:
-        import errno
-        if not exist_ok or e.errno != errno.EEXIST:
-            raise
-
 if __name__ == "__main__":
     # To make tests repeatable
     tf.set_random_seed(1)
     np.random.seed(1)
 
     #test_onevox(BiExpModel({}))
-    #test_multivox(BiExpModel({}))
-    test_nifti("test_data_exp.nii", BiExpModel({}), 0, 0.02, "noisy")
+    test_multivox(BiExpModel({}))
 
