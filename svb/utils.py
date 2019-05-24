cc97335bfb2ad5b0460c4b2cc60cc4ff58a45135
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-05-24 15:06:40 +0100

    Remove old debug method

--- svb/utils.py
+++ svb/utils.py
@@ -9,7 +9,7 @@ class LogBase(object):
     """
     Base class that provides a named log and the ability to log tensors easily
     """
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.log = logging.getLogger(type(self).__name__)
 
     def log_tf(self, tensor, level=logging.DEBUG, **kwargs):
@@ -33,24 +33,3 @@ class LogBase(object):
                             summarize=kwargs.get("summarize", 100))
         else:
             return tensor
-
-def debug(obj, tensor, **kwargs):
-    """
-    Log a tensor if obj.debug = True
-
-    :param tensor: tf.Tensor
-
-    Keyword arguments:
-
-    :param summarize: Number of entries to include (default 100)
-    :param force: If True, always log this tensor regardless of log level
-    :param shape: If True, precede tensor with its shape
-    """
-    if kwargs.get("force", False) or obj.debug:
-        items = [tensor]
-        if kwargs.get("shape", False):
-            items.insert(0, tf.shape(tensor))
-        return tf.Print(tensor, items, "\n%s" % kwargs.get("name", tensor.name),
-                        summarize=kwargs.get("summarize", 100))
-    else:
-        return tensor
