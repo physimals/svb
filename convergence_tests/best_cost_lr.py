398effc17adfb0aea3dd923d936fc7309cd08fa7
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-06-27 11:59:08 +0100

    Include plots for variable number of time points

--- convergence_tests/best_cost_lr.py
+++ convergence_tests/best_cost_lr.py
@@ -10,23 +10,25 @@ import matplotlib.pyplot as plt
 
 LEARNING_RATES = (1.0, 0.5, 0.25, 0.1, 0.05, 0.02, 0.01)
 BATCH_SIZES = (5, 10, 15, 25, 50)
-NT = 20
+NT = (10, 20, 50, 100)
 
-for bs in BATCH_SIZES:
-    if bs > NT: continue
-    best_cost = []
-    label = "Batch size=%i" % bs
-    for lr in LEARNING_RATES:
-        subdir = "nt_%i_lr_%.3f_bs_%i" % (NT, lr, bs)
-        cost_history = nib.load(os.path.join(subdir, "cost_history.nii.gz")).get_data()
-        best_cost.append(np.mean(cost_history, axis=(0, 1, 2))[-1])
-    plt.plot(best_cost, label=label)
+for idx, nt in enumerate(NT):
+    plt.subplot(2, 2, idx+1)
+    for bs in BATCH_SIZES:
+        if bs > nt: continue
+        best_cost = []
+        label = "Batch size=%i" % bs
+        for lr in LEARNING_RATES:
+            subdir = "nt_%i_lr_%.3f_bs_%i" % (nt, lr, bs)
+            cost_history = nib.load(os.path.join(subdir, "cost_history.nii.gz")).get_data()
+            best_cost.append(np.mean(cost_history, axis=(0, 1, 2))[-1])
+        plt.plot(best_cost, label=label)
 
-plt.title("Best cost by learning rate and batch size")
-plt.ylabel("Best cost achieved")
-plt.xlabel("Learning rate")
-#plt.ylim(35, 100)
-plt.xticks(range(len(LEARNING_RATES)), ["%.2f" % lr for lr in LEARNING_RATES])
-plt.legend()
+    plt.title("Best cost by learning rate and batch size (NT=%i)" % nt)
+    plt.ylabel("Best cost achieved")
+    plt.xlabel("Learning rate")
+    #plt.ylim(35, 100)
+    plt.xticks(range(len(LEARNING_RATES)), ["%.2f" % lr for lr in LEARNING_RATES])
+    plt.legend()
 
 plt.show()
--- convergence_tests/conv_lr.py
+++ convergence_tests/conv_lr.py
@@ -9,18 +9,26 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 
 LEARNING_RATES = (1.0, 0.5, 0.25, 0.1, 0.05, 0.02, 0.01, 0.005)
+#BATCH_SIZES = (5, 10, 15, 25, 50)
+NT = (10, 20, 50, 100)
+BS = 10
 
-for lr in LEARNING_RATES:
-    subdir = "lr_%.3f_bs_10" % lr
-    label = "%.3f" % lr
-    cost_history = nib.load(os.path.join(subdir, "cost_history.nii.gz")).get_data()
-    plt.plot(np.mean(cost_history, axis=(0, 1, 2)), label=label)
+for idx, nt in enumerate(NT):
+    plt.subplot(2, 2, idx+1)
+    y0 = 1e10
+    for lr in LEARNING_RATES:
+        subdir = "nt_%i_lr_%.3f_bs_%i" % (nt, lr, BS)
+        label = "LR: %.3f" % lr
+        cost_history = nib.load(os.path.join(subdir, "cost_history.nii.gz")).get_data()
+        plt.plot(np.mean(cost_history, axis=(0, 1, 2)), label=label)
+        if y0 > np.min(cost_history):
+            y0 = np.min(cost_history)
 
-plt.title("Convergence of VB-I-4 and VB-NI-4 analysis by average free energy")
-plt.yscale("symlog")
-plt.ylabel("Cost")
-plt.xlabel("Epochs")
-plt.ylim(35, 200)
-plt.legend()
+    plt.title("Convergence of average free energy by learning rate (NT=%i)" % nt)
+    #plt.yscale("symlog")
+    plt.ylabel("Cost")
+    plt.xlabel("Epochs")
+    plt.ylim(y0, y0*10)
+    plt.legend()
 
 plt.show()
