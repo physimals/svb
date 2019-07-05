da0957dc0a049ed818f22639054c7457fcd43f01
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-07-05 15:20:25 +0100

    Expose distributions and model base class

--- svb/__init__.py
+++ svb/__init__.py
@@ -4,5 +4,7 @@ Implementation of Stochastic Variational Bayesian inference for fitting timeseri
 
 from svb._version import __version__, __timestamp__
 from svb.svb import SvbFit
+import svb.dist as dist
+import svb.model as model
 
-__all__ = ["__version__", "__timestamp__", "SvbFit"]
+__all__ = ["__version__", "__timestamp__", "SvbFit", "dist", "model"]
