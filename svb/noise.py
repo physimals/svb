7f1c0bd5eccdf6ea1c1b8569ae011f651cd20b05
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-10-15 10:30:27 +0100

    Update naming to be consistent with other bits

--- svb/noise.py
+++ svb/noise.py
@@ -38,21 +38,20 @@ class NoiseParameter(Parameter):
         """
         nvoxels = tf.shape(data)[0]
         batch_size = tf.shape(data)[1]
-        draw_size = tf.shape(pred)[1]
+        sample_size = tf.shape(pred)[1]
 
-        log_noise = noise # for clarity
-        #log_noise = tf.fill([nvoxels, draw_size], 0.0)
-        data = self.log_tf(tf.tile(tf.reshape(data, [nvoxels, 1, batch_size]), [1, draw_size, 1], name="data"))
+        log_noise = self.log_tf(noise, name="noise_samples") # for clarity FIXME use parameter transforms
+        data = self.log_tf(tf.tile(tf.reshape(data, [nvoxels, 1, batch_size]), [1, sample_size, 1], name="data"))
         pred = self.log_tf(pred)
 
-        # Square_diff has shape [NV, D, B]
+        # Square_diff has shape [NV, S, B]
         square_diff = self.log_tf(tf.square(data - pred, name="square_diff"))
 
         # Since we are processing only a batch of the data at a time, we need to scale this term
         # correctly to the latent loss
         scale = self.log_tf(tf.divide(tf.to_float(nt), tf.to_float(batch_size), name="scale"))
 
-        # Log likelihood has shape [NV, D]
+        # Log likelihood has shape [NV, S]
         noise_var = self.log_tf(tf.exp(log_noise, name="noise_var"))
         log_likelihood = 0.5 * (log_noise * tf.to_float(nt) +
                                 scale * tf.reduce_sum(square_diff, axis=-1) / noise_var)
