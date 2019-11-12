844af54a0084ad22bf2c94e72d0aed192d72da82
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-11-12 14:05:16 +0000

    Simplified to use svb.main.run() and include covariance run initialized with previous posterior

--- examples/biexp_example.py
+++ examples/biexp_example.py
@@ -1,17 +1,11 @@
 import sys
-import logging
 import math
 
 import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
 
-from svb import SvbFit, dist, DataModel
-from svb.models.exp import BiExpModel
-from svb.main import setup_logging
-
-log = logging.getLogger(__name__)
-setup_logging(tee=sys.stdout)
+from svb.main import run
 
 # Set the properties of the simulated data
 num_times=200
@@ -43,66 +37,48 @@ data_noisy = data + np.random.normal(0, true_params["noise_sd"], size=shape)
 nii_noisy = nib.Nifti1Image(data_noisy, np.identity(4))
 nii_noisy.to_filename(name + "_noisy.nii.gz")
 
-# Create the forward model
-fwd_model = BiExpModel(dt=dt)
-log.info("Created model: %s", str(fwd_model))
-for option in fwd_model.OPTIONS:
-    log.info(" - %s: %s", option.desc, str(getattr(fwd_model, option.attr_name)))
-
-# Initialize the data model which contains data dimensions, number of time
-# points, list of unmasked voxels, etc
-data_model = DataModel(name + "_noisy.nii.gz")
-data_model.nifti_image(data_model.data_flattened).to_filename("example_data.nii.gz")
-
-# Get the time points from the model
-tpts = fwd_model.tpts(data_model)
-if tpts.ndim > 1 and tpts.shape[0] > 1:
-    tpts = tpts[data_model.mask_flattened > 0]
-
 # Train model without covariance
 options = {
     "learning_rate" : 0.005,
     "batch_size" : 10,
     "sample_size" : 10,
     "epochs" : 500,
+    "log_stream" : sys.stdout,
+    "save_mean" : True,
+    "save_var" : True,
+    "save_param_history" : True,
+    "save_cost" : True,
+    "save_cost_history" : True,
+    "save_model_fit" : True,
+    "save_log" : True,
 }
-svb = SvbFit(data_model, fwd_model, **options)
-log.info("Training model...")
-ret = svb.train(tpts, data_model.data_flattened, **options)
 
-# Get output, transposing as required so first index is by parameter
-mean_cost_history = ret[0]
-cost_history_v = ret[2]
-param_history_v = ret[3]
-modelfit = ret[4]
+runtime, svb, training_history = run(data_noisy, "biexp", "example_out", **options)
+mean_cost_history = training_history["mean_cost"]
+cost_history_v = training_history["voxel_cost"]
+param_history_v = training_history["voxel_params"]
+modelfit = svb.evaluate(svb.modelfit)
 means = svb.evaluate(svb.model_means)
-variances = np.transpose(svb.evaluate(svb.post.var))
+variances = svb.evaluate(svb.model_vars)
+post_mean = svb.evaluate(svb.post.mean)
+post_cov = svb.evaluate(svb.post.cov)
 
-# Train model with covariance
-svb = SvbFit(data_model, fwd_model, infer_covar=True, **options)
-log.info("Training model (with covariance)...")
-ret = svb.train(tpts, data_model.data_flattened, **options)
-
-# Get output, transposing as required so first index is by parameter
-mean_cost_history_cov = ret[0]
-cost_history_v_cov = ret[2]
-param_history_v_cov = ret[3]
-modelfit_cov = ret[4]
+runtime, svb, training_history = run(data_noisy, "biexp", "example_out_cov", infer_covar=True, **options)
+mean_cost_history_cov = training_history["mean_cost"]
+cost_history_v_cov = training_history["voxel_cost"]
+param_history_v_cov = training_history["voxel_params"]
+modelfit_cov = svb.evaluate(svb.modelfit)
 means_cov = svb.evaluate(svb.model_means)
-variances_cov = np.transpose(svb.evaluate(svb.post.var))
+variances_cov = svb.evaluate(svb.model_vars)
 
-# Write out parameter mean and variance images plus param history
-for idx, param in enumerate(svb.params):
-    data_model.nifti_image(means[idx]).to_filename("mean_%s.nii.gz" % param.name)
-    data_model.nifti_image(variances[idx]).to_filename("var_%s.nii.gz" % param.name)
-    data_model.nifti_image(param_history_v[:, :, idx]).to_filename("mean_%s_history.nii.gz" % param.name)
-    
-# Write out voxelwise cost history
-data_model.nifti_image(cost_history_v[..., -1]).to_filename("cost.nii.gz")
-data_model.nifti_image(cost_history_v).to_filename("cost_history.nii.gz")
-
-# Write out modelfit
-data_model.nifti_image(modelfit).to_filename("modelfit.nii.gz")
+runtime, svb, training_history = run(data_noisy, "biexp", "example_out_cov_init", infer_covar=True, 
+                                     initial_posterior=(post_mean, post_cov), **options)
+mean_cost_history_cov_init = training_history["mean_cost"]
+cost_history_v_cov_init = training_history["voxel_cost"]
+param_history_v_cov_init = training_history["voxel_params"]
+modelfit_cov_init = svb.evaluate(svb.modelfit)
+means_cov_init = svb.evaluate(svb.model_means)
+variances_cov_init = svb.evaluate(svb.model_vars)
 
 if "--show" in sys.argv:
     # Plot some results
@@ -110,23 +86,39 @@ if "--show" in sys.argv:
 
     for idx in range(num_examples):
         plt.figure(1)
+        plt.suptitle("Cost history (No covariance)")
         ax1 = plt.subplot(sq_len, sq_len, idx+1)
         plt.plot(cost_history_v[idx])
 
         plt.figure(2)
+        plt.suptitle("Cost history (With covariance)")
         ax1 = plt.subplot(sq_len, sq_len, idx+1)
         plt.plot(cost_history_v_cov[idx])
 
         plt.figure(3)
+        plt.suptitle("Cost history (With covariance, pre-initialized)")
         ax1 = plt.subplot(sq_len, sq_len, idx+1)
-        plt.plot(tpts, data_model.data_flattened[idx],'rx')
-        plt.plot(tpts, clean_data[idx], 'r')
-        plt.plot(tpts, modelfit[idx],'b')
+        plt.plot(cost_history_v_cov_init[idx])
 
         plt.figure(4)
+        plt.suptitle("Model fit (No covariance)")
         ax1 = plt.subplot(sq_len, sq_len, idx+1)
-        plt.plot(tpts, data_model.data_flattened[idx],'rx')
-        plt.plot(tpts, clean_data[idx], 'r')
-        plt.plot(tpts, modelfit_cov[idx],'b')
+        plt.plot(svb.data_model.data_flattened[idx],'rx')
+        plt.plot(clean_data[idx], 'r')
+        plt.plot(modelfit[idx],'b')
+
+        plt.figure(5)
+        plt.suptitle("Model fit (With covariance)")
+        ax1 = plt.subplot(sq_len, sq_len, idx+1)
+        plt.plot(svb.data_model.data_flattened[idx],'rx')
+        plt.plot(clean_data[idx], 'r')
+        plt.plot(modelfit_cov[idx],'b')
+
+        plt.figure(6)
+        plt.suptitle("Model fit (With covariance, pre-initialized)")
+        ax1 = plt.subplot(sq_len, sq_len, idx+1)
+        plt.plot(svb.data_model.data_flattened[idx],'rx')
+        plt.plot(clean_data[idx], 'r')
+        plt.plot(modelfit_cov_init[idx],'b')
 
     plt.show()
