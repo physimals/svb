a285425a92391759a592be860730215cdac99a39
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-12-10 11:48:44 +0000

    Add 'misc' models for testing - constant signal and polynomial

--- svb/models/__init__.py
+++ svb/models/__init__.py
@@ -3,6 +3,7 @@ Base classes and built in models for SVB inference
 """
 from .asl import *
 from .exp import *
+from .misc import *
 
 def get_model_class(model_name):
     """
@@ -16,5 +17,9 @@ def get_model_class(model_name):
         return ExpModel
     elif model_name == "biexp":
         return BiExpModel
+    elif model_name == "constant":
+        return ConstantModel
+    elif model_name == "poly":
+        return PolyModel
     else:
         raise ValueError("No such model: %s" % model_name)
