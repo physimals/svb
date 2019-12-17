4a595a527b2b4d2bb1b03a60d5bfe6fc00620451
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-12-17 12:08:26 +0000

    Improved labelling for paper

--- biexp_tests/prior_post_infonly.py
+++ biexp_tests/prior_post_infonly.py
@@ -1,5 +1,5 @@
 """
-Comparison best free energy by prior/posterior
+Comparison best free energy by prior/posterior with only informative posteriors used
 """
 
 import os
@@ -12,24 +12,20 @@ import matplotlib.pyplot as plt
 BASEDIR="c:/Users/ctsu0221/dev/data/svb/biexp/"
 
 readable_labels = {
-    "i_i" : "Inf/Default",
-    "i_i_init" : "Inf/Data",
-    "i_i_true" : "Inf/True",
-    "i_i_wrong" : "Inf/Wrong",
-    "i_ni" : "Inf/Noninf",
-    "i_ni_init" : "Inf/Noninf-Data",
-    "ni_i" : "Noninf/Default",
-    "ni_i_init" : "Noninf/Data",
-    "ni_i_true" : "Noninf/True",
-    "ni_i_wrong" : "Noninf/Wrong",
-    "ni_ni" : "Noninf/Noninf",
-    "ni_ni_init" : "Noninf/Noninf-Data",
+#    "i_i" : "Informative/Default",
+    "i_i_init" : "Informative/Data",
+    "i_i_true" : "Informative/True",
+    "i_i_wrong" : "Informative/Wrong",
+#    "ni_i" : "Noninformative/Default",
+    "ni_i_init" : "Noninformative/Data",
+    "ni_i_true" : "Noninformative/True",
+    "ni_i_wrong" : "Noninformative/Wrong",
 }
 
-plt.figure(figsize=(10, 16))
+plt.figure(figsize=(8, 16))
 ordered_labels = None
 for idx, suffix in enumerate(("_analytic", "_num", "_analytic_corr", "_num_corr")):
-    subdirs = [d for d in glob.glob(os.path.join(BASEDIR, "prior*")) if os.path.isdir(d) and d.endswith(suffix)]
+    subdirs = [d for d in glob.glob(os.path.join(BASEDIR, "prior*")) if os.path.isdir(d) and d.endswith(suffix) and "post_ni" not in d and ("init" in d or "true" in d or "wrong" in d)]
     labels = [os.path.basename(s).replace("prior_", "").replace("post_", "").replace("_analytic", "").replace("_num", "").replace("_corr", "") for s in subdirs]
     cases = dict(zip(labels, subdirs))
     best_cost = []
@@ -44,15 +40,15 @@ for idx, suffix in enumerate(("_analytic", "_num", "_analytic_corr", "_num_corr"
 
     plt.subplot(4, 1, idx+1)
     plt.boxplot(best_cost, labels=ordered_labels, showfliers=False)
-    plt.title("Best free energy by prior/posterior options: %s" % suffix)
-    plt.ylabel("Best free energy achieved")
+    plt.xticks(fontsize=8) 
+    plt.title("Best cost by prior/posterior options: %s" % suffix)
+    plt.ylabel("Best cost achieved")
     plt.xlabel("Prior/initial posterior")
-    plt.xticks(fontsize=6) 
-    plt.ylim(0, 550)
+    plt.ylim(40, 100)
     #plt.yscale("symlog")
     plt.legend()
 
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.4)
-plt.savefig("prior_post.png")
+plt.savefig("prior_post_infonly.png")
 plt.show()
