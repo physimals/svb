3f93bc9a24a82751de0dbfe595fef1f575e6fef5
Martin Craig <martin.craig@eng.ox.ac.uk>
2020-02-03 11:02:05 +0000

    Enable code to be imported without autogenerated version/timestamp Mostly for readthedocs but can also occur if pip installed without setup.py being run

--- svb/__init__.py
+++ svb/__init__.py
@@ -1,8 +1,12 @@
 """
 Implementation of Stochastic Variational Bayesian inference for fitting timeseries data
 """
-
-from ._version import __version__, __timestamp__
+try:
+    from ._version import __version__, __timestamp__
+except ImportError
+    __version__ = "Unknown version"
+    __timestamp__ = "Unknown timestamp"
+    
 from .svb import SvbFit
 from .data import DataModel
 from .model import Model
