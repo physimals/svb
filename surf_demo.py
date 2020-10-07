799719cae703089e1a621e44117d2bc8e1c785c9
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2020-10-07 12:51:09 +0100

    before noise work

--- surf_demo.py
+++ surf_demo.py
@@ -1,10 +1,16 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
-# %%
-from IPython import get_ipython
+# %% [markdown]
+# # SVB surface test script
+# 
+# This script demonstrates SVB running 'on the surface' to infer the parameters of a single exponential decay signal model. 
+# 
+# Voxel-wise data is generated with a given amplitude, decay rate and noise SD. Each voxel in the volume is an independent time series of samples. Although they all share the same underlying signal parameters, the additive zero-mean (aka white) noise present in each one will be different. 
+# 
+# A spherical surface is then generated to intersect the voxel grid (note that many voxels will not intersect the surface and are therefore discarded). Multiple surface vertices may be present in each voxel, leading to an under-determined system. The SVB framework is used to infer the signal parameters at each vertex *on the surface* using the data generated in *volume space*. The process is run both with and without the spatial prior to illustrate how this is helpful in an under-determined system. 
 
-
 # %%
+import os
 import sys
 import math
 import numpy as np
@@ -12,9 +18,9 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 import toblerone as tob 
 import regtricks as rt
-import pyvista as pv 
 import trimesh
 from svb.main import run
+import pickle
 
 try:
     import tensorflow.compat.v1 as tf
@@ -26,102 +32,151 @@ except ImportError:
 tf.set_random_seed(1)
 np.random.seed(1)
 
-pv.set_plot_theme('document')
-
+# %% [markdown]
+# Generate a cubic voxel grid of data. The signal parameters for the exponential model are amplitude and decay rate, and the noise is zero-mean with a given SD. Multiple samples are taken from the exponential curve to produce a 4D dataset (X,Y,Z,t)
 
 # %%
 # Set the properties of the simulated data
 num_times = 200
-sq_len = 5
-vox_size = 2 
+sq_len = 5 
+vox_size = 2
 true_params = {
     "amp1" : 10.0, # the true amplitude of the model
     "r1" : 3.0,    # the true decay rate
     "noise_sd" : 0.5,
 }
 true_var = true_params["noise_sd"]**2
-
-# For time being, make sure we have a good range of samples based on the choice of R1
 dt = 5.0/(true_params["r1"] * num_times)
 
-
-# %%
-# Generate test data and write to filenames
 shape = (sq_len, sq_len, sq_len, num_times)
 data = np.zeros(shape)
 for t_idx in range(num_times):
     t = t_idx*dt
     data[..., t_idx] = true_params["amp1"]*math.exp(-true_params["r1"]*t)
-data_noisy = data + np.random.normal(0, true_params["noise_sd"], size=shape)
-data_noisy.shape
+data_noisy_nopv = data + np.random.normal(0, true_params["noise_sd"], size=shape)
+print('Data shape', data_noisy_nopv.shape)
 
+# %% [markdown]
+# Create a reference voxel grid for the data 
+# 
 
 # %%
-# Create a reference voxel grid for the data 
 ref_spc = rt.ImageSpace.create_axis_aligned(np.zeros(3), 3 * [sq_len], 3 * [vox_size])
-
-# Generate the point cloud of voxel centres
-vox_cents = ref_spc.voxel_centres().reshape(-1,3)
-voxel_poly = pv.PolyData(vox_cents)
 ref_spc
 
-# %%
-# Generate a spherical surface
-sph_mesh = trimesh.creation.icosphere(2, ((sq_len * vox_size)/2.2))
-surf = tob.Surface.manual(sph_mesh.vertices + (sq_len * vox_size)/2,    
-                                sph_mesh.faces)
-
+# %% [markdown]
+# Generate a spherical surface, with radius slightly smaller than 1/2 the FoV of the reference space
+# 
 
 # %%
-# Form the weighting matrix for surface -> voxels
-# We will use weighted averaging (no PV weighting etc)
-surf2vol_weights = np.zeros((ref_spc.size.prod(), surf.points.shape[0]))
-vertices_ijk = rt.aff_trans(ref_spc.world2vox, surf.points).round(0)
-vertices_inds = np.ravel_multi_index(vertices_ijk.T.astype('i4'), ref_spc.size)
-for vtx_number, vtx_ind in enumerate(vertices_inds):
-    surf2vol_weights[vtx_ind, vtx_number] = 1 
+ctx_thickness = 1
+mid_r = (sq_len * vox_size)/2.3
+in_r = mid_r - ctx_thickness/2
+out_r = mid_r + ctx_thickness/2
+mesh = trimesh.creation.icosphere(2, 1) 
+in_surf, mid_surf, out_surf = [
+    tob.Surface.manual((r * mesh.vertices) + (sq_len * vox_size)/2, mesh.faces) for r in [in_r, mid_r, out_r]
+]
+print(in_surf)
+print(mid_surf)
+print(out_surf)
 
-assert (surf2vol_weights.sum(0) == 1).all()
-divisor = surf2vol_weights.sum(1)
-mask = (divisor > 0)
-surf2vol_weights[surf2vol_weights == 0] = -1
-surf2vol_weights[mask,:] /= divisor[mask,None]
-surf2vol_weights[surf2vol_weights < 0] = 0
-surf2vol_weights = surf2vol_weights[mask,:]
-assert (surf2vol_weights.sum(1) == 1).all()
+# %% [markdown]
+# Calculate the mean vertex spacing on the mesh. As the Laplacian is a function of the second derivative, we may expect the smoothing parameter ak to vary in proportion to (1/length scale)^2?
+
+# %%
+mid_edges = mid_surf.edges()
+vertex_dist = np.linalg.norm(mid_edges, axis=-1).reshape(-1,3).mean()
+print("Mean vertex spacing on midsurface {:.2f}mm".format(vertex_dist))
+
+# %% [markdown]
+# Form the weighting matrix for surface to volume projection. The projection incorporates PV effects, hence why the input data must be scaled with PVs. 
+
+# %%
+hemi = tob.Hemisphere.manual(in_surf, out_surf, 'L')
+if os.path.exists('proj.pkl'):
+    with open('proj.pkl', 'rb') as f: 
+        projector = pickle.load(f)
+else: 
+    projector = tob.projection.Projector(hemi, ref_spc)
+    with open('proj.pkl', 'wb') as f: 
+        pickle.dump(projector, f)
+projector.surf2vol_matrix().shape
+
+# %% [markdown]
+# Mask out voxels with no vertices within. A cortical surface at 32k resolution in a voxel grid of ~3mm iso has around 10 vertices per voxel. 
+
+# %%
+vertices_per_voxel = (projector.surf2vol_matrix() > 0).sum(1).A.flatten()
+vol_mask = (vertices_per_voxel > 0)
+surf2vol_weights = projector.surf2vol_matrix()[vol_mask,:]
+print("Mean vertices per voxel:", (surf2vol_weights > 0).sum(1).mean())
 
 
 # %%
-# Train model without covariance
+pvs = projector.flat_pvs()
+data_noisy = data_noisy_nopv * pvs[:,0].reshape(ref_spc.size)[...,None]
+# plt.figure(figsize=(8,4))
+# plt.subplot(1,2,1)
+# plt.title('Noisy data, no PVE')
+# plt.imshow(data_noisy_nopv[:,:,5,0], vmin=data_noisy_nopv.min(), vmax=data_noisy_nopv.max())
+# plt.subplot(1,2,2)
+# plt.title('Noisy data, with PVE')
+# plt.imshow(data_noisy[:,:,5,0], vmin=data_noisy_nopv.min(), vmax=data_noisy_nopv.max())
+# plt.show()
+
+# %% [markdown]
+# Plot the surface to volume projection matrix
+
+# %%
+# plt.figure(figsize=(5,5))
+# plt.spy(surf2vol_weights, markersize=0.5)
+# plt.title('Surface to volume projection matrix')
+# plt.xlabel('Vertex number')
+# plt.ylabel('Voxel index (masked)')
+# plt.show()
+
+
+# %% [markdown]
+# Repeat the inference process, this time with the spatial prior enabled for amplitude and decay. This enforces similarity between the estimates of neighbouring vertices. 
+
+# %%
 options = {
     "learning_rate" : 0.005,
     "batch_size" : 10,
-    "sample_size" : 8,
-    "epochs" : 500,
+    "sample_size" : 5,
+    "epochs" : 1000,
     "log_stream" : sys.stdout,
     "n2v" : surf2vol_weights,
     "prior_type": "N",
-    "param_overrides": {
-        "amp1": { "prior_type": "M" } 
-        } 
+    "ak": 30
 }
 
-
-# %%
 runtime, svb, training_history = run(
     data_noisy, "exp", 
     "example_out_cov", 
-    mask=mask,
-    surfaces={'LMS': surf}, 
-    infer_covar=True, 
+    mask=vol_mask,
+    surfaces={'LMS': mid_surf}, 
+    dt=dt,
     **options)
 
-mean_cost_history_cov = training_history["mean_cost"]
-cost_history_v_cov = training_history["voxel_cost"]
-param_history_v_cov = training_history["voxel_params"]
-modelfit_cov = svb.evaluate(svb.modelfit)
-means_cov = svb.evaluate(svb.model_means)
-variances_cov = svb.evaluate(svb.model_vars)
+mean_cost_history = training_history["mean_cost"]
+cost_history_v = training_history["voxel_cost"]
+param_history_v = training_history["voxel_params"]
+modelfit = svb.evaluate(svb.modelfit)
+means = svb.evaluate(svb.model_means)
+variances = svb.evaluate(svb.model_vars)
+
+# %% [markdown]
+# For each parameter of interest, we infer a mean (best estimate) and variance (degree of uncertainty). We also project the surface parameter estimates back into volume space for reference. 
+
+# %%
+amp1_mean, r1_mean, noise_mean = [ means[d] for d in range(3) ]
+amp1_var, r1_var, noise_var =  [ variances[0] for d in range(3) ] 
+vol_amp1 = surf2vol_weights.dot(amp1_mean)
+
+# %% [markdown]
+
 
 
 # %%
