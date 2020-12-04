44e3367621c6fa42b05e5a75384db97d616fa78e
tomfrankkirk <tomfrankkirk@gmail.com>
2020-12-04 11:05:23 +0000

    Checkpoint: pure surface demo works with new projector-only interface.

--- surf_demo.py
+++ surf_demo.py
@@ -2,7 +2,6 @@
 
 # %%
 import os
-import pickle
 import sys 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,8 +9,11 @@ import toblerone as tob
 import regtricks as rt
 import trimesh
 from svb.main import run
+import pyvista as pv 
+from pyvista import PlotterITK
 from svb_models_asl import AslRestModel 
 from svb.data import SurfaceModel, VolumetricModel
+import pickle
 
 
 try:
@@ -24,18 +26,21 @@ except ImportError:
 tf.set_random_seed(1)
 np.random.seed(1)
 
+
 sq_len = 10 
 vox_size = 2
 ref_spc = rt.ImageSpace.create_axis_aligned(np.zeros(3), 3 * [sq_len], 3 * [vox_size])
 bounding_box = np.array([ref_spc.bbox_origin, ref_spc.bbox_origin + ref_spc.fov_size])
 bounding_box = np.meshgrid(*[ se for se in bounding_box.T ])
+ref_spc
 
-ctx_thickness = 2
+
+ctx_thickness = 1
 out_r = ((sq_len * vox_size) - 1) / 2
 in_r = out_r - ctx_thickness
 mid_r = out_r - (ctx_thickness / 2)
 mesh = trimesh.creation.icosphere(2, 1)
-shift = 0.75 * (np.random.rand(*mesh.vertices.shape) - 0.5)
+shift = 1.5 * (np.random.rand(*mesh.vertices.shape) - 0.5)
 orig = (ref_spc.fov_size / 2)
 
 out_surf = tob.Surface.manual((out_r * mesh.vertices) + orig + shift, mesh.faces)
@@ -43,10 +48,13 @@ in_surf = tob.Surface.manual((in_r * mesh.vertices) + orig + shift, mesh.faces)
 mid_surf = tob.Surface.manual((mid_r * mesh.vertices) + orig + shift, mesh.faces)
 
 print(mid_surf)
+mid_edges = mid_surf.edges()
+vertex_dist = np.linalg.norm(mid_edges, axis=-1).reshape(-1,3).mean()
+print("Mean vertex spacing on midsurface {:.2f}mm".format(vertex_dist))
 
 
 # %%
-hemi = tob.Hemisphere.manual(in_surf, out_surf, 'L')
+hemi = tob.Hemisphere(in_surf, out_surf, 'L')
 if not os.path.exists('proj.pkl'):
     projector = tob.projection.Projector(hemi, ref_spc)
     with open('proj.pkl', 'wb') as f:
@@ -57,43 +65,44 @@ else:
 
 # %%
 vertices_per_voxel = (projector.surf2vol_matrix(True) > 0).sum(1).A.flatten()
-vol_mask = projector.flat_pvs()[:,:2].any(1)
+vol_mask = (vertices_per_voxel > 0)
 surf2vol_weights = projector.surf2vol_matrix(True)[vol_mask,:]
 print("Mean vertices per voxel:", (surf2vol_weights > 0).sum(1).mean())
+
+from scipy import sparse
+gp = mid_surf.mesh_laplacian(distance_weight=1)
+
+def sym(a):
+    return not ((np.abs(a - a.T) >= 1e-6).max())
+
+def is_nsd(a):
+    return not (sparse.linalg.eigs(a)[0] > 0).any()
+
+assert sym(gp)
+assert is_nsd(gp)
 
 plds = np.arange(0.25, 1.75, 0.25)
 repeats = 8
 data = np.zeros(3*[sq_len] + [ repeats * plds.size ])
 mask = vol_mask.reshape(data.shape[:3])
+surf_model = SurfaceModel(data, surfaces={'LMS': mid_surf}, projector=projector, mask=mask)
+asl_model = AslRestModel(surf_model, plds=plds, repeats=repeats, casl=True) 
 
-cortex_model = AslRestModel(
-        SurfaceModel(data, surfaces={'LMS': mid_surf}, projector=projector), 
-        plds=plds, repeats=repeats, casl=True) 
-subcortex_model = AslRestModel(
-            VolumetricModel(data), plds=plds, repeats=repeats, casl=True)
-
-GM_CBF = 60 
-WM_CBF = 20
+CBF = 60 
 ATT = 0.75
 NOISE_VAR = 1
 
-with tf.Session() as sess:
-    tpts = cortex_model.tpts()
-    cortex_data = sess.run(cortex_model.evaluate([
-        GM_CBF * np.ones([mid_surf.points.shape[0], 1], dtype=np.float32),
-        ATT * np.ones([mid_surf.points.shape[0], 1], dtype=np.float32)
-    ], tpts))
+ftiss = CBF * np.ones([surf_model.n_nodes, 1], dtype=np.float32)
+deltiss = ATT * np.ones([surf_model.n_nodes, 1], dtype=np.float32)
+tpts = asl_model.tpts()
 
 with tf.Session() as sess:
-    tpts = subcortex_model.tpts()
-    subcortex_data = sess.run(subcortex_model.evaluate([
-        WM_CBF * np.ones([mask.size, 1], dtype=np.float32),
-        ATT * np.ones([mask.size, 1], dtype=np.float32)
-    ], tpts))
+    surf_data = sess.run(asl_model.evaluate([ftiss, deltiss], tpts))
 
-vol_data = projector.surf2vol(cortex_data, edge_scale=True)
-vol_data += (subcortex_data * projector.flat_pvs()[:,1,None])
+vol_data = projector.surf2vol(surf_data, edge_scale=True)
 vol_data += np.random.normal(0, NOISE_VAR, vol_data.shape)
+pvs = projector.flat_pvs()
+vox_idx = np.argmax(pvs[:,0])
 vol_data = vol_data.reshape(*ref_spc.size, tpts.shape[-1])
 
 # Fit options common to both runs 
@@ -106,7 +115,6 @@ options = {
     "prior_type": "N",
     "mask" : mask,
     "projector" : projector,
-    "surfaces" : { 'LMS': mid_surf }, 
     "plds": plds, 
     "repeats": repeats, 
     "casl": True, 
